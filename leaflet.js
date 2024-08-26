b0d05d11dfff2478c65b4ac47cc95f788779379c
Kiran Anand <liteninkiran@hotmail.com>
2024-08-26 20:56:28 +0100

    005 - Shapes & Basics

--- leaflet.js
+++ leaflet.js
@@ -1,28 +1,33 @@
-const mapOptions = {
-    zoomControl: false,
-}
+const mapOptions = { }
 const circleOptions = {
     color: 'red',
     fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500,
+    fillOpacity: 0.2,
+    radius: 200,
 }
 const tileOptions = {
     maxZoom: 19,
 }
+const myCoords = [
+    50.78070645717412,
+    -1.0774538641364018,
+];
 const polygonCoords = [
-    [51.509, -0.080],
-    [51.503, -0.060],
-    [51.511, -0.047],
+    [myCoords[0] - 0.006, myCoords[1] + 0.001],
+    [myCoords[0] + 0.002, myCoords[1] + 0.004],
+    [myCoords[0] - 0.003, myCoords[1] + 0.007],
 ];
 
-const map = L.map('map', mapOptions).setView([51.505, -0.09], 13);
-const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl, tileOptions).addTo(map);
-const marker = L.marker([51.5, -0.09]).addTo(map).bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
-const circle = L.circle([51.508, -0.11], circleOptions).addTo(map).bindPopup('I am a circle.');
+const map = L.map('map', mapOptions).setView(myCoords, 16);
+const tileUrls = [
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+    'https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey={apikey}',
+];
+const tiles = L.tileLayer(tileUrls[0], tileOptions).addTo(map);
+const marker = L.marker(myCoords).addTo(map).bindPopup('<b>Hello world!</b><br />I am a popup.');
+const circle = L.circle(myCoords, circleOptions).addTo(map).bindPopup('I am a circle.');
 const polygon = L.polygon(polygonCoords).addTo(map).bindPopup('I am a polygon.');
-const popup = L.popup().setLatLng([51.513, -0.09]).setContent('I am a standalone popup.').openOn(map);
 const onMapClick = (e) => popup.setLatLng(e.latlng).setContent(`You clicked the map at ${e.latlng.toString()}`).openOn(map);
 
 map.on('click', onMapClick);
